8966b50478e7c67aa784554a4e5f85e71022f2d6
Shun40 <shun.no40@gmail.com>
2013-11-11 00:07:45 +0900

    物語作成画面全体の背景クラスを修正

--- script/class/story/ScreenBackground.js
+++ script/class/story/ScreenBackground.js
@@ -1,10 +1,6 @@
-var ScreenBackground = enchant.Class.create(enchant.Sprite, {
+var ScreenBackground = enchant.Class.create(StorySceneSprite, {
 	initialize : function(path, width, height, x, y, parent) {
-		enchant.Sprite.call(this, width, height);
-		this.image = core.assets[path];
-		this.x = x;
-		this.y = y;
-		this._parent = parent;
+		StorySceneSprite.call(this, path, width, height, x, y, parent);
 	},
 
 	//　暗い色に変更する
@@ -14,14 +10,5 @@ var ScreenBackground = enchant.Class.create(enchant.Sprite, {
 		} else if(isDark === false) {
 			this.image = core.assets[STORY_BACKGROUND._path];
 		}
-	},
-
-	// 物語作成画面へ加える
-	_addToStoryScene : function() {
-		storyScene.addChild(this);
-	},
-	// 物語作成画面から削除する
-	_removeFromStoryScene : function() {
-		storyScene.removeChild(this);
 	},
 })
