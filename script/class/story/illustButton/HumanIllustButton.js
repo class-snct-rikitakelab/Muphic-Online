be3e031718f7139657f601a16e4606ed63c84ef6
Shun40 <shun.no40@gmail.com>
2013-11-10 23:37:49 +0900

    「はいけい」、「じんぶつ」、「どうぶつ」、「あいてむ」イラストボタンのクラスから余計なプロパティとメソッドを削除

--- script/class/story/illustButton/HumanIllustButton.js
+++ script/class/story/illustButton/HumanIllustButton.js
@@ -7,7 +7,6 @@ var HumanIllustButton = enchant.Class.create(enchant.Sprite, {
 		this.y = y;
 		// 以下, このクラスのプロパティ
 		this._isPush = false; // ボタンが押されているかどうか
-		this._canTouch = true; // ボタンが押せるかどうか
 		this._parent = parent; // このクラスの親にあたるオブジェクト
 	},
 
@@ -15,20 +14,13 @@ var HumanIllustButton = enchant.Class.create(enchant.Sprite, {
 	_setIsPush : function(isPush) {
 		this._isPush = isPush;
 	},
-
 	// touchEnabledプロパティにブーリアン値をセットする
 	_setTouchEnabled : function(touchEnabled) {
 		this.touchEnabled = touchEnabled;
 	},
-
 	// opacityプロパティのブーリアン値をセットする
 	_setOpacity : function(opacity) {
 		this.opacity = opacity;
-	},
-
-	//ボタンが押せない状態にする
-	_setTouchEnable : function (canTouch) {
-		this._canTouch = canTouch;
 	},
 
 	// On, Off時の画像切り替え
@@ -39,7 +31,6 @@ var HumanIllustButton = enchant.Class.create(enchant.Sprite, {
 			this.image = core.assets[STORY_HUMAN_ILLUSTBUTTON_OFF._path];
 		}
 	},
-
 	//　ボタンが押せない（暗い）状態の色に変更する
 	_setDarkImage : function(isDark) {
 		if(isDark === true) {
@@ -47,18 +38,6 @@ var HumanIllustButton = enchant.Class.create(enchant.Sprite, {
 		} else if(isDark === false) {
 			this.image = core.assets[STORY_HUMAN_ILLUSTBUTTON_OFF._path];
 		}
-	},
-
-	// ボタンの全てのステータスを押せる状態に合わせる
-	_setCanPush : function() {
-		this._setTouchEnabled(true);
-		this._setOpacity(1.0);
-	},
-
-	// ボタンの全てのステータスを押せない状態に合わせる
-	_setCannotPush : function() {
-		this._setTouchEnabled(false);
-		this._setOpacity(0.5);
 	},
 
 	// クリック時の処理
